5213983f845383e5b11e7817c8e8beefb601a861
Alex <ais_alex@ukr.net>
2023-04-29 11:30:30 +0300

    summary

--- helpers/sendEmail_sendGrid.js
+++ helpers/sendEmail_sendGrid.js
@@ -5,18 +5,6 @@ const { SENDGRID_API_KEY, EMAIL_FROM } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-// const email = {
-//   to: "", // кому
-//   from: EMAIL_FROM, // от кого
-//   subject: "Test email", // тема письма
-//   html: `<p>Test email</p>`,
-// };
-
-// sgMail
-//   .send(email)
-//   .then(() => console.log("Email send success"))
-//   .catch((error) => console.log(error.message));
-
 const sendEmailSendGrid = async (data) => {
   const email = { ...data, from: EMAIL_FROM };
   await sgMail.send(email);
